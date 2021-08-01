acf87b36010e32f4ee054629a71ad54c4bfb7bff
luoxing1998 <72681791+luoxing1998@users.noreply.github.com>
2021-08-01 20:01:41 +0800

    Update GitTest.java
    测试pull

--- src/main/java/com/eustc/git/GitTest.java
+++ src/main/java/com/eustc/git/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("test pull");
+        System.out.println("test push");
     }
 
     public static void testLog(){
