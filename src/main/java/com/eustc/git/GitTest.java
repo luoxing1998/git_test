4dbd103ed4b35294a3ccc2127a49bfad38e2428b
luoxing1998 <luoxing@mail.ustc.edu.cn>
2021-08-01 11:34:34 +0800

    first commit

---
+++ src/main/java/com/eustc/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.eustc.git;
+
+/**
+ * @author luoxing
+ * @create 2021-08-01 11:29
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+    }
+}
