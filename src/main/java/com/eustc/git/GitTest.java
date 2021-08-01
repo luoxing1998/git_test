244c5aae67e1b742b7c4365387b2cdaeddab87b4
luoxing1998 <luoxing@mail.ustc.edu.cn>
2021-08-01 20:00:10 +0800

    测试push

--- src/main/java/com/eustc/git/GitTest.java
+++ src/main/java/com/eustc/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("hot-fix第一次提交");
         System.out.println("hot-fix");
         System.out.println("master");
+        System.out.println("test pull");
     }
 
     public static void testLog(){
