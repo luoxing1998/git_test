05e7f151b70c02a9a633987972cb72cfc97be343
luoxing1998 <luoxing@mail.ustc.edu.cn>
2021-08-01 11:38:45 +0800

    second commit

--- src/main/java/com/eustc/git/GitTest.java
+++ src/main/java/com/eustc/git/GitTest.java
@@ -7,5 +7,10 @@ package com.eustc.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试git");
+        testLog();
+    }
+
+    public static void testLog(){
+        System.out.println("第二次提交");
     }
 }
