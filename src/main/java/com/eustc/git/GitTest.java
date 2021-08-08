9a3dd0f0bc9782df88472dace7c798528bfc25b3
luoxing1998 <luoxing@mail.ustc.edu.cn>
2021-08-08 21:20:11 +0800

    cool 分支 first commit

--- src/main/java/com/eustc/git/GitTest.java
+++ src/main/java/com/eustc/git/GitTest.java
@@ -25,6 +25,7 @@ public class GitTest {
         System.out.println("再次测试test pull");
         System.out.println("两个分支的测试");
         System.out.println("测试新分支：lx_testGit");
+        System.out.println("cool分支");
         testJSONObject();
     }
 
